79a261ca1f74488af9b941c09639be72c0822344
jjeka <jekanikitenko@gmail.com>
2016-10-12 02:25:37 +0300

    Updated test and test build

--- tests/test.c
+++ tests/test.c
@@ -1,16 +1,40 @@
-void x(int a);
+#define BEGIN() asm volatile("mov $0177774, sp\nmov $0177774, r5\nnop");
+#define END() 										\
+	int __end = 2;									\
+	asm volatile("halt");							\
+	asm volatile("mov %0, r4\nhalt" :: "r"(__end));
+
+volatile unsigned char* vram = (volatile unsigned char*) (32768);
+
+volatile unsigned char* addr; 
 
 void _start()
 {
-	int a=2+4;
-	int b =1 * 2 *3 * a;
+	BEGIN();
 
-	x(a);
+	int tt = 1;
+	while (1)
+	{
+		int at = tt % 500;
+		int bt = tt / 500;
+		int t = (bt%2) ? tt : (500 - tt);
+
+		int x;
+		for (x = 0; x < 100; x++)
+		{
+			int y;
+			for (y = 0; y < 100; y++)
+			{
+				//*(addr + (y * 100 + x)) = t * ((50 - x) * (50 - x) + (50 - y) * (50 - y));
+				int a = t * ((50 - x) * (50 - x) + (50 - y) * (50 - y))%255;
+				int b = t * ((50 - x) * (50 - x) + (50 - y) * (50 - y))/255;
+				*(vram + (y * 100 + x)) = (b%2) ? a : (255-a);
+			}
+		}
+
+		tt++;
+	}
+
+	END();
 }
 
-void x(int a)
-{
-	asm("nop\n"
-		"nop\n"
-		"nop");
-}
