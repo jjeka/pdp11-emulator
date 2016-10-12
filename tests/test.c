c47caf97fd917ced526c4223b62fa9eae910835d
jjeka <jekanikitenko@gmail.com>
2016-10-13 00:13:39 +0300

    Updated test

--- tests/test.c
+++ tests/test.c
@@ -1,17 +1,26 @@
-#define BEGIN() asm volatile("mov $0177774, sp\nmov $0177774, r5");
-#define END() 										\
-	int __end = 0;									\
-	asm volatile("halt");							\
-	asm volatile("mov %0, r4\nhalt" :: "r"(__end));
-
 volatile unsigned char* vram = (volatile unsigned char*) (32768);
 
-volatile unsigned char* addr; 
+volatile unsigned char* get_vram();
+void exec();
 
 void _start()
 {
-	BEGIN();
+	asm volatile("mov $0177776, sp\nmov $0177776, r5");
 
+	exec();
+
+	int __end = 0;
+	asm volatile("halt");
+	asm volatile("mov %0, r4\nhalt\njmp $0" :: "r"(__end));
+}
+
+volatile unsigned char* get_vram()
+{
+	return vram;
+}
+
+void exec()
+{
 	int tt = 1;
 	while (1)
 	{
@@ -27,13 +36,10 @@ void _start()
 			{
 				int a = t * ((50 - x) * (50 - x) + (50 - y) * (50 - y)) % 255;
 				int b = t * ((50 - x) * (50 - x) + (50 - y) * (50 - y)) / 255;
-				*(vram + (y * 100 + x)) = (b%2) ? a : (255 - a);
+				*(get_vram() + (y * 100 + x)) = (b%2) ? a : (255 - a);
 			}
 		}
 
 		tt++;
 	}
-
-	END();
 }
-
